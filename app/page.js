0692d29f07f4c6c2d443aed9b59de7ce1088aaa2
Nick <nicholasterehin@gmail.com>
2025-03-14 18:23:10 -0400

    Added dyanamic styling for main component and added ChromeLinkcomponent within the main element

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Header from "../components/header";
 import Body from "../components/body";
 import Footer from "../components/footer";
+import ChromeLink from "../components/chromeLink";
 
 const Page = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col w-full">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow max-w-5xl mx-auto px-4 sm:px-6 md:px-8 w-full">
         <Body />
+        <ChromeLink />
       </main>
       <Footer />
     </div>
