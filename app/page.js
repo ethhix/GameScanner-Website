c2bce07b288fbb33919ca3ffc407f22136a89f4d
Nick <nicholasterehin@gmail.com>
2025-03-21 17:46:53 -0400

    Removed header and footer from page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,13 @@
-import Header from "../components/header";
 import Body from "../components/body";
-import Footer from "../components/footer";
 import ChromeLink from "../components/chromeLink";
 
 const Page = () => {
   return (
-    <div className="min-h-screen flex flex-col w-full">
-      <Header />
+    <div className="h-fit mb-8 flex flex-col w-full">
       <main className="flex-grow max-w-5xl mx-auto px-4 sm:px-6 md:px-8 w-full">
         <Body />
         <ChromeLink />
       </main>
-      <Footer />
     </div>
   );
 };
