bc5446824639b95a3bc8e23d7a5f31dc802f3a4f
Nick <nicholasterehin@gmail.com>
2025-03-21 17:42:22 -0400

    Changed header to be a link that redirects beck to the homepage

--- components/header.js
+++ components/header.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
+import Link from "next/link";
 
 const Header = () => {
   useEffect(() => {
@@ -19,9 +20,11 @@ const Header = () => {
     <div className="w-full px-4 sm:px-6">
       <header className="flex justify-center p-2 sm:p-4 md:p-6">
         <div className="relative flex flex-col justify-center max-w-fit">
-          <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-8xl jersey-20-regular">
-            GameScanner Extension
-          </h1>
+          <Link href={"/"}>
+            <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-8xl jersey-20-regular">
+              GameScanner Extension
+            </h1>
+          </Link>
           <h3 className="sub-text absolute -right-2 sm:-right-4 md:-right-8 lg:-right-14 bottom-1 text-[#AC81FF] text-xl sm:text-2xl md:text-3xl lg:text-5xl jersey-10-regular transform translate-y-1/4 -rotate-25">
             For Twitch!
           </h3>
