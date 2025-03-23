17fd2a5d2d07b4308eb535288d36c44bc671ff56
Nick <nicholasterehin@gmail.com>
2025-03-23 17:39:59 -0400

    Added some new configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 86400,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  headers: async () => [
+    {
+      source: "/:all*(svg|jpg|png|webp|avif)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=31536000, immutable",
+        },
+      ],
+    },
+  ],
+};
